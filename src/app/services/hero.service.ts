68ff8a0cce18e59b39f3464b10c6c2e794113f75
ugeHidalgo <ugeHidalgo@hotmail.com>
2018-01-20 17:40:45 +0100

    Added error handling in get and getById service methods

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from '../models/hero';
 import { MessageService } from './message.service';
@@ -20,23 +21,43 @@ export class HeroService {
   private heroesServiceUrl = 'api/heroes';
 
   getHero(id: number): Observable<Hero> {
-    this.log(`Hero with id ${id} was fetched.`)
-    let hero = this.http.get<Hero>(`${this.heroesServiceUrl}/${id}`);
+    let me = this,
+        getHeroByIdUrl = `${me.heroesServiceUrl}/${id}`,
+        hero = me.http.get<Hero>(getHeroByIdUrl)
+                      .pipe(
+                        tap(hero => me.log(`Hero with id ${id} was fetched.`)),
+                        catchError(me.handleError<Hero>(`getHero (id:${id}`, null))
+                      );
     return hero;
   }
 
   getHeroes(): Observable<Hero[]> {
-    var me = this;
-
-    me.log('Heroes fetched.');
-    let heroesFromApi = me.http.get<Hero[]>(me.heroesServiceUrl);
-    return heroesFromApi;
+    let me = this,
+        heroes = me.http.get<Hero[]>(me.heroesServiceUrl)
+                  .pipe(
+                    tap(heroes => me.log('Heroes fetched.')),
+                    catchError(this.handleError('getHeroes', []))
+                  );
+    return heroes;
   }
 
 
   //Private methods
   private log(message: string): void {
     this.messageService.add('HeroService: ' + message );
+  }
+
+  /**
+   * 
+   * @param operation - name of the operation that failed.
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T){
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
   }
 
 }
