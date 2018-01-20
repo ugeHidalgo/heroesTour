5edc3292dfa5622a51c0224e6b3a7d427d92a0bb
ugeHidalgo <ugeHidalgo@hotmail.com>
2018-01-20 17:17:24 +0100

    Usesd an in-memory api instead of mocking data in an array.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule  } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http'
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/hero-list/heroes.component';
 import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MessagesComponent } from './messages/messages.component';
 
 import { HeroService } from './services/hero.service';
 import { MessageService } from './services/message.service';
-import { MessagesComponent } from './messages/messages.component';
-import { AboutComponent } from './about/about.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
@@ -27,7 +31,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot ( InMemoryDataService, {dataEncapsulation: false})
   ],
 
   providers: [ 
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -2,25 +2,41 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Hero } from '../models/hero';
-import { HEROES } from '../mockedData/mock-heroes';
 import { MessageService } from './message.service';
 
 
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
+
+  //URL to heroes web API (Actually it´s not a real web api, it´s a in-memory web API)
+  private heroesServiceUrl = 'api/heroes';
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: hero with id ${id} was fetched.`)
-    var hero = HEROES.find(hero => hero.id == id);
-    return of(hero);
+    this.log(`Hero with id ${id} was fetched.`)
+    let hero = this.http.get<Hero>(`${this.heroesServiceUrl}/${id}`);
+    return hero;
   }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: Heroes fetched.');
-    return of(HEROES);
+    var me = this;
+
+    me.log('Heroes fetched.');
+    let heroesFromApi = me.http.get<Hero[]>(me.heroesServiceUrl);
+    return heroesFromApi;
+  }
+
+
+  //Private methods
+  private log(message: string): void {
+    this.messageService.add('HeroService: ' + message );
   }
 
 }
